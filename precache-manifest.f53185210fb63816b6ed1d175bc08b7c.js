c34d4c514e91f224e73440bb9219748046e64033
Tylopilus <elitaer@gmail.com>
2019-04-01 19:12:08 +0200

    new try

---
+++ precache-manifest.f53185210fb63816b6ed1d175bc08b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0488aedd4825e7880085",
+    "url": "/static/js/main.489ef657.chunk.js"
+  },
+  {
+    "revision": "a0a6accbe918189e12b5",
+    "url": "/static/js/2.cc4ac0a3.chunk.js"
+  },
+  {
+    "revision": "0488aedd4825e7880085",
+    "url": "/static/css/main.17e63cf8.chunk.css"
+  },
+  {
+    "revision": "1a702ed5792f4efcdfc2f4da72fc8ee1",
+    "url": "/index.html"
+  }
+];
