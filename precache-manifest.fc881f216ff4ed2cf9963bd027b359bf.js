5f4e98cdb54e410297fcf741c661eb882d257847
Tylopilus <elitaer@gmail.com>
2019-04-01 19:09:42 +0200

    forgot files to add

---
+++ precache-manifest.fc881f216ff4ed2cf9963bd027b359bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "60ea2f790b4eead92919",
+    "url": "/static/js/main.8ba05927.chunk.js"
+  },
+  {
+    "revision": "a0a6accbe918189e12b5",
+    "url": "/static/js/2.cc4ac0a3.chunk.js"
+  },
+  {
+    "revision": "60ea2f790b4eead92919",
+    "url": "/static/css/main.17e63cf8.chunk.css"
+  },
+  {
+    "revision": "5bd2d28e97c74b9c609228b53cd3be19",
+    "url": "/index.html"
+  }
+];
